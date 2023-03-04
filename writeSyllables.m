4eae99c37b53d08f47a95b219a228c8b96db6b57
benpolletta <benpolletta@gmail.com>
2023-03-04 09:47:22 -0500

    Refactoring pound replacement code in writeSyllables.

--- writeSyllables.m
+++ writeSyllables.m
@@ -53,6 +53,7 @@ for s = 1:length(tsylb_output)
     empty_word_indicator = cellfun(@(x) length(x) == 0, word_cell);
     word_cell(empty_word_indicator) = [];
     word_cell = cellfun(@strip, word_cell, 'unif', 0);
+    word_num = length(word_cell);
     
     %% Splitting words into syllables.
     word_sylb_cell = cellfun(@(x) strsplit(x, {'[', '] ', ']'}, 'CollapseDelimiters', true), word_cell, 'unif', false);
@@ -85,83 +86,130 @@ for s = 1:length(tsylb_output)
 
     %% Fixing words containing pauses transcribed as #.
 
-    if any(strcmp(phone_cell, '#'))
+    transcript = wt_data(s).pronunciations;
+    transcript = transcript(~cellfun(@(x) cellfun(@(y) isempty(y), x), transcript));
+    t_words = cat(2, transcript{:});
+    t_pound_count = cellfun(@(x) any(strcmp(x, '#')), t_words);
 
-        transcript = wt_data(s).pronunciations;
-        transcript = transcript(~cellfun(@(x) cellfun(@(y) isempty(y), x), transcript));
-        transcript_phone_counts = cellfun(@(x) cellfun(@(y) length(y), x), transcript)';
-        split_bool = false;
+    if any(t_pound_count)
 
-        if length(transcript) == length(word_phone_num)
+        t_phone_count = cellfun(@length, t_words);
+        t_comp_count = t_phone_count - t_pound_count;
+        cum_t_count = cumsum([0 t_comp_count]);
 
-            split_bool = any(transcript_phone_counts - word_phone_num ~= 0);
+        wsp_words = cellfun(@(x) cat(2, x{:}), word_sylb_phone_cell, 'unif', 0);
+        wsp_phones = cat(2, wsp_words{:});
+        cum_w_count = cumsum(word_phone_num);
 
-        end
+        if length(t_words) == word_num - 1;
 
-        if length(transcript_phone_counts) ~= length(word_phone_num) || split_bool
+            %% Assuming that a word is split in two by the #.
 
-            % Getting index of #.
-%             pound_word_index = find(cellfun(@(x) any(strcmp(x{:}, '#')), transcript));
-            pound_phone_index = find(strcmp(phone_cell, '#'));
-            pound_phone_index = pound_phone_index - (0:(length(pound_phone_index) - 1));
+            for i = 1:length(transcript)
 
-            % Retrieving start and end indices of split words.
-            word_end_phone = min(cumsum(word_phone_num), num_phones);
-            word_start_phone = min([0, word_end_phone((1:(end - 1)))] + 1, num_phones);
-            word_phone_boundaries = [word_start_phone; word_end_phone]';
-
-%             ts_end_phone = min(cumsum(transcript_phone_counts), num_phones);
-%             ts_start_phone = min([0, ts_end_phone((1:(end - 1)))] + 1, num_phones);
-%             ts_phone_boundaries = [ts_start_phone; ts_end_phone]';
-
-            for p = 1:length(pound_phone_index)
-
-%                 split_phone_boundaries = ts_phone_boundaries(pound_word_index, :);
-% 
-%                 split_word_index = any(word_phone_boundaries <= split_phone_boundaries(2) & word_phone_boundaries >= split_phone_boundaries(1), 2);
-                pound_adjacent = any(word_phone_boundaries <= (pound_phone_index(p) + 1) & word_phone_boundaries >= (pound_phone_index(p) - 1), 2);
-                pa_indices = find(pound_adjacent);
-
-                up_to = min([pa_indices; length(word_phone_num); length(transcript)]);
-                
-                if any(word_phone_num(1:up_to) ~= transcript_phone_counts(1:up_to))
-
-%                     if word_phone_num(up_to) = (transcript_phone_counts(up_to) - 1)
-%                         
-%                         word_sylb_phone_cell{{up_to}}{1}{end + 1} = '#'; 
-%                     
-%                     else
-
-                        % Recombining split words;
-                        split_word = cat(2, word_sylb_phone_cell(pound_adjacent));
-                        new_syl = {split_word{1}{end}, {'#'}, split_word{2}{1}};
-                        new_syl = cat(2, new_syl{:});
-                        new_word = {split_word{1}{1:(end - 1)}, new_syl, split_word{2}{2:end}};
-
-                        % Replacing split words with new word.
-                        word_sylb_phone_cell{pa_indices(1)} = new_word;
-                        word_sylb_phone_cell(pa_indices(2)) = [];
-
-%                     end
-
-                    % Recounting syllables and phones per word.
-                    word_sylb_phone_num = cellfun(@(x) cellfun(@length, x), word_sylb_phone_cell, 'unif', 0);
-                    word_phone_num = cellfun(@sum, word_sylb_phone_num);
-
-                    % Adjusting pound_index.
-                    pound_phone_index = pound_phone_index + (cumsum(ones(size(pound_phone_index))) > p);
-
-                end
+                this_word_indicator = cum_w_count <= cum_t_count(i + 1) & cum_w_count > cum_t_count(i);
+                wsp2t_map(this_word_indicator) = i;
 
             end
 
-            for_print = cellfun(@(x) cellfun(@(y) strjoin(y, '/'), x, 'unif', 0), word_sylb_phone_cell, 'unif', 0);
-            for_print = cellfun(@(x) strjoin(x, '*'), for_print, 'unif', 0);
-            for_print = strjoin(for_print,'|');
+            pound_adjacent = wsp2t_map == find(t_pound_count);
+            pa_indices = find(pound_adjacent);
 
-            fprintf(pound_fid, '%s\n%s\n%s\n', filename, sentence_tsylb, for_print);
+            % Recombining split words;
+            split_word = cat(2, word_sylb_phone_cell(pound_adjacent));
+            new_syl = {split_word{1}{end}, {'#'}, split_word{2}{1}};
+            new_syl = cat(2, new_syl{:});
+            new_word = {split_word{1}{1:(end - 1)}, new_syl, split_word{2}{2:end}};
+
+            % Replacing split words with new word.
+            word_sylb_phone_cell{pa_indices(1)} = new_word;
+            word_sylb_phone_cell(pa_indices(2)) = [];
+
+        elseif length(t_words) ~= word_num
+
+            display(sprintf('Error w/ %s: transcript has %d words & word_sylb_phone_cell has %d words.', filename, length(t_words), word_num))
+
+        elseif any(t_phone_count - word_phone_num == 1)
+
+            % Finding word that's affected.
+            pound_adjacent = t_phone_count - word_phone_num == 1;
+            pa_index = find(pound_adjacent);
+            poundless_word = word_sylb_phone_cell(pound_adjacent);
+
+            % Finding phone indices of that word.
+            word_phone_indices = mat2cell(1:length(wsp_phones), 1, word_phone_num);
+            word_sylb_phone_indices = cellfun(@(x, y) mat2cell(x, 1, y), word_phone_indices, word_sylb_phone_num, 'unif', 0);
+            poundless_indices = word_sylb_phone_indices{pound_adjacent};
+
+            t_phones = cat(2, t_words{:});
+            pound_index = find(strcmp(t_phones, '#'), 1);
+            
+            % Adding phone to beginning or end of word.
+            new_word = poundless_word;
+            if pound_index < min(poundless_indices{:})
+                new_word = cat(2, {'#'}, new_word{1}{:});
+            elseif pound_index > max(poundless_indices{:})
+                new_word = cat(2, new_word{1}{:}, {'#'});
+            end
+
+            % Replacing poundless word with new word.
+            word_sylb_phone_cell{pa_index} = {new_word};
+
+        elseif any(t_phone_count ~= word_phone_num)
+
+            display(sprintf('Error w/ %s: transcript phones per word:', filename))
+            t_phone_count
+            display('word_sylb_phone_cell phones per word:')
+            word_phone_num
 
         end
+
+        %             % Strategy is to line up two phoneme strings (one from word_sylb_cell & one from transcript),
+        %             % deleting from word_sylb_cell & inserting from transcript as needed.
+        %             % Getting two strings of phonemes.
+        %
+        %             % Getting cumulative phone counts w/in each syllable.
+        %             sylb_ends = cumsum(cat(2, word_sylb_phone_num{:}));
+        %             sylb_starts = [1, sylb_ends(1:(end - 1)) + 1];
+        %             sylb_bounds = [sylb_starts; sylb_ends]';
+        %             word_sylb_bounds = mat2cell(sylb_bounds, cellfun(@length, word_sylb_phone_cell), 2); % middle argument b/c word_sylb_num doesn't include zero-syllable words
+        %
+        %             pound_index = find(strcmp(t_phones, '#'), 1);
+        %             pound_sylb_index = cellfun(@(x) x(:, 1) <= pound_index - 1 & x(:, 2) >= pound_index - 1, word_sylb_bounds, 'UniformOutput', 0);
+        %             pa_bounds = cellfun(@(x, y) x(y, :), word_sylb_bounds, pound_adjacent);
+        %
+        %             % Are these syllables part of the same word?
+        %
+        %
+        %             % Retrieving start and end indices of split words.
+        %             word_end_phone = min(cumsum(word_phone_num), num_phones);
+        %             word_start_phone = min([0, word_end_phone((1:(end - 1)))] + 1, num_phones);
+        %             word_phone_boundaries = [word_start_phone; word_end_phone]';
+        %
+        %             ts_end_phone = min(cumsum(transcript_phone_counts), num_phones);
+        %             ts_start_phone = min([0, ts_end_phone((1:(end - 1)))] + 1, num_phones);
+        %             ts_phone_boundaries = [ts_start_phone; ts_end_phone]';
+
+        %                     if word_phone_num(up_to) = (transcript_phone_counts(up_to) - 1)
+        %
+        %                         word_sylb_phone_cell{{up_to}}{1}{end + 1} = '#';
+        %
+        %                     else
+        %                     end
+
+        % Recounting syllables and phones per word.
+        word_sylb_phone_num = cellfun(@(x) cellfun(@length, x), word_sylb_phone_cell, 'unif', 0);
+        word_phone_num = cellfun(@sum, word_sylb_phone_num);
+
+%         % Adjusting pound_index.
+%         pound_phone_index = pound_phone_index + (cumsum(ones(size(pound_phone_index))) > p);
+
+        % Printing out fix.
+        for_print = cellfun(@(x) cellfun(@(y) strjoin(y, '/'), x, 'unif', 0), word_sylb_phone_cell, 'unif', 0);
+        for_print = cellfun(@(x) strjoin(x, '*'), for_print, 'unif', 0);
+        for_print = strjoin(for_print,'|');
+
+        fprintf(pound_fid, '%s\n%s\n%s\n', filename, sentence_tsylb, for_print);
 
     end
 
